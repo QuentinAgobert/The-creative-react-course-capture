4913e2f4c1963173e74480680fc4a9f59019eb59
Quentin Agobert <70266879+QuentinAgobert@users.noreply.github.com>
2020-11-13 19:52:39 +0100

    11. Movie Details

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -12,21 +12,21 @@ const OurWork = () => {
             <Movie>
                 <h2>The Athlete</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/the-athlete">
                     <img src={athlete} alt="The Athlete"/>
                 </Link>
             </Movie>
             <Movie>
                 <h2>The Racer</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/the-racer">
                     <img src={theracer} alt="The Racer"/>
                 </Link>
             </Movie>
             <Movie>
                 <h2>Good Times</h2>
                 <div className="line"></div>
-                <Link>
+                <Link to="/work/good-times">
                     <img src={goodtimes} alt="Good Times"/>
                 </Link>
             </Movie>
@@ -38,7 +38,6 @@ const Work = styled.div`
     min-height: 100vh;
     overflow: hidden;
     padding: 5rem 10rem;
-    background: white;
     h2 {
         padding: 1rem 0rem;
     }
