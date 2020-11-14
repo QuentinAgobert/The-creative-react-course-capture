00486e7eeb90d4e63302bb42d94a0d36d2285933
Quentin Agobert <70266879+QuentinAgobert@users.noreply.github.com>
2020-11-14 14:45:47 +0100

    13. Page Transitions

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,38 +5,22 @@ import { About, Description, Image, Hide } from '../styles';
 import { motion } from 'framer-motion';
 
 const AboutSection = () => {
-    const titleAnim = {
-        hidden: { opacity: 0 },
-        show: { opacity: 1, transition: { duration: 2 } }
-    };
-    const container = {
-        hidden: { x: 100 },
-        show: { 
-            x: 0, 
-            transition: { 
-                duration: 0.75, 
-                ease: 'easeOut', 
-                staggerChildren: 0.25, 
-                when: 'beforeChildren' 
-            } 
-        },
-    };
     return (
         <About>
             <Description>
-                <motion.div variants={container} initial="hidden" animate="show" className="title">
+                <motion.div>
                     <Hide>
-                        <motion.h2 variants={titleAnim}>
+                        <motion.h2>
                             We work to make
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 variants={titleAnim}>
+                        <motion.h2>
                             your <span>dreams</span>
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 variants={titleAnim}>
+                        <motion.h2>
                             come true.
                         </motion.h2>
                     </Hide>
