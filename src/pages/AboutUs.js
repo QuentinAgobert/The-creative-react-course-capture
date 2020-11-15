b575653d176e4244c17cc1d688cf3a5225fcd388
Quentin Agobert <70266879+QuentinAgobert@users.noreply.github.com>
2020-11-15 16:44:35 +0100

    22. Fixing Weird Quirks

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,6 +3,7 @@ import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import FaqSection from '../components/FaqSection';
+import ScrollTop from './components/ScrollTop';
 // Animations
 import { motion } from 'framer-motion';
 import { pageAnimation } from '../animation';
@@ -14,6 +15,7 @@ const AboutUs = () => {
             <AboutSection />
             <ServicesSection />
             <FaqSection />
+            <ScrollTop />
         </motion.div>
     );
 };
